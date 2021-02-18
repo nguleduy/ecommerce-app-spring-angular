73e7d313c578c05705f3285b9e42b0e40ece0aa3
Joseph <nguld@itsj-group.com>
2021-02-18 21:56:31 +0700

    BE: add BaseEntity

--- backend/src/main/java/com/ecommerce/backend/model/BaseEntity.java
+++ backend/src/main/java/com/ecommerce/backend/model/BaseEntity.java
@@ -1,7 +1,16 @@
 package com.ecommerce.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity implements Serializable {
 
   private Long id;
