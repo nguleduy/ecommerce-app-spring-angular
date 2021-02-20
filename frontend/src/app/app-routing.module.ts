a7885afd5bdcf0d4645cdf6cdf09b5dfe22c2c0b
Joseph <nguld@itsj-group.com>
2021-02-20 23:24:11 +0700

    FE: add routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProductsComponent} from './component/products/products.component';
+import {CategoryComponent} from './component/category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductsComponent},
+  {path: '', component: ProductsComponent},
+  {path: '**', component: CategoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
